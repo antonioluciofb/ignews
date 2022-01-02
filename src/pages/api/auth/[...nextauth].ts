7c26f6d4553f7b097684d9b964df3c5779427d46
Antonio Lucio <antonioluciofb@hotmail.com>
2022-01-02 20:29:01 -0300

    Added FaunaDB

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,7 @@
 import NextAuth from "next-auth";
 import Providers from "next-auth/providers";
-
+import { Fauna } from "../../../services/fauna";
+import { query as q } from "faunadb";
 export default NextAuth({
   // Configure one or more authentication providers
   providers: [
@@ -11,4 +12,23 @@ export default NextAuth({
     }),
     // ...add more providers here
   ],
+  callbacks: {
+    async signIn({ name, email }: any) {
+      try {
+        // await Fauna.query(q.Create(q.Collection("users"), { data: { name, email } }));
+        await Fauna.query(
+          q.If(
+            q.Not(q.Exists(q.Match(q.Index("user_by_email"), q.Casefold(email)))),
+            q.Create(q.Collection("users"), { data: { name, email } }),
+            q.Get(q.Match(q.Index("user_by_email"), q.Casefold(email)))
+          )
+        );
+      } catch (error) {
+        console.log("error ============>", error);
+        return false;
+      }
+
+      return true;
+    },
+  },
 });
